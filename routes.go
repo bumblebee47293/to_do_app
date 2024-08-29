d75a6065538b214eb6497b0813ebb180d1450e44
Vasil <bumblebee47293@gmail.com>
2024-08-29 07:44:51 +0530

    Add routes

---
+++ routes.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/todos", getTodos)
+	router.POST("/todos", createTodo)
+	router.PUT("/todos/:id", updateTodo)
+	router.DELETE("/todos/:id", deleteTodo)
+
+	router.Run(":8080")
+}
